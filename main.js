a129cddfb6deaff6d21afce06bfff87c3e537f08
Marina <marina.lsz522@gmail.com>
2024-12-27 01:02:53 +0100

    Creados nuevos botones

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Impportaciones
 import "./style.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
@@ -5,8 +6,10 @@ import Footer from "./components/Footer/Footer";
 import toastr from "toastr";
 import "toastr/build/toastr.min.css";
 
+//Clave para utilizar la API de Unsplash
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
+//Renderizar la página web
 const buildWebsite = () => {
   Header();
   Main();
@@ -14,51 +17,73 @@ const buildWebsite = () => {
   getPhotos("candy", 10, "relevant", "magenta", "landscape");
 };
 
-const getPhotos = async (keyword, photoNum, order, color, orientation) => { 
+//Función para obtener las fotos
+const getPhotos = async (keyword, photoNum, order, color, orientation) => {
   const container = document.querySelector("#gallery");
   const message = document.querySelector("#message");
+
+  //Mensaje que se muestra mientras se cargan las fotos
   message.textContent = "Searching for photos...";
   container.innerHTML = "";
+
+  //Construcción de la URL de la API de Unsplash de manera dinámica con los parámetros de búsqueda
+  const url = new URL("https://api.unsplash.com/search/photos");
+  url.searchParams.append("page", 1);
+  if (keyword) url.searchParams.append("query", keyword);
+  if (photoNum) url.searchParams.append("per_page", photoNum);
+  if (order) url.searchParams.append("order_by", order);
+  if (color) url.searchParams.append("color", color);
+  if (orientation) url.searchParams.append("orientation", orientation);
+  url.searchParams.append("client_id", CLIENT_ID);
+
+  //Petición a la API de Unsplash
   try {
-    const data = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${keyword}&per_page=${photoNum}&order_by=${order}&color=${color}&orientation=${orientation}&client_id=${CLIENT_ID}`
-    );
+    const data = await fetch(url.toString());
     const dataJSON = await data.json();
     const photos = dataJSON.results;
     printPhotos(photos);
   } catch (error) {
+    //Mensaje que se muestra si hay un error al cargar las fotos
     message.textContent = "Something went wrong. Please try again!";
     const li = document.createElement("li");
     li.innerHTML = `
       <img src="/error.png" alt="Error" id="errorImg" />
     `;
     container.appendChild(li);
+    container.style.display = "flex";
+    container.style.justifyContent = "center";
+    container.style.alignItems = "center";
     const errorImg = document.getElementById("errorImg");
     errorImg.style.boxShadow = "none";
+    errorImg.style.width = "300px";
+    errorImg.style.height = "300px";
     console.error(error);
   }
 };
 
+//Función para immprimir las fotos en la página
 const printPhotos = (photos) => {
   const main = document.querySelector("main");
   const container = document.querySelector("#gallery");
   const message = document.querySelector("#message");
+
+  //Si no se encuentran fotos, se muestra lo siguiente
   if (photos.length === 0) {
     container.innerHTML = "";
     message.textContent =
       "No results yet, but inspiration is right around the corner!";
     const li = document.createElement("li");
+    li.className = "centered-content";
     li.innerHTML = `
         <img src="/character.png" alt="Flower retro character" id="characterImg" />
       `;
     container.appendChild(li);
-    container.style.display = "flex";
-    container.style.justifyContent = "center";
-    container.style.alignItems = "center";
     const characterImg = document.getElementById("characterImg");
     characterImg.style.boxShadow = "none";
     characterImg.style.width = "300px";
     characterImg.style.height = "300px";
+
+    //Sugerencias de búsqueda
     const suggestions = ["sun", "moon", "sea"];
     const suggestionsContainer = document.createElement("div");
     suggestionsContainer.id = "suggestions";
@@ -75,6 +100,7 @@ const printPhotos = (photos) => {
     `;
     main.appendChild(suggestionsContainer);
 
+    //Evento para buscar por sugerencia
     document.querySelectorAll(".suggestion-btn").forEach((button) => {
       button.addEventListener("click", (event) => {
         const keyword = event.target.getAttribute("data-keyword");
@@ -87,6 +113,7 @@ const printPhotos = (photos) => {
       });
     });
   } else {
+    //Si se encuentran fotos, se muestran en la página
     container.innerHTML = "";
     message.textContent = `I've found ${photos.length} photos:`;
     for (const photo of photos) {
@@ -107,12 +134,21 @@ const printPhotos = (photos) => {
 
 buildWebsite();
 
+document.querySelector("#resetFiltersBtn").addEventListener("click", () => {
+  document.querySelector("#countInput").value = "";
+  document.querySelector("#orderInput").value = "";
+  document.querySelector("#colorInput").value = "";
+  document.querySelector("#orientationInput").value = "";
+});
+
 document.querySelector("#searchBtn").addEventListener("click", () => {
-  const keywordValue = document.querySelector("#searchInput").value.trim();
-  const photoNumValue = document.querySelector("#countInput").value;
-  const orderValue = document.querySelector("#orderInput").value;
-  const colorValue = document.querySelector("#colorInput").value || "";;
-  const orientationValue = document.querySelector("#orientationInput").value;
+  const keywordValue =
+    document.querySelector("#searchInput").value.trim() || "";
+  const photoNumValue = document.querySelector("#countInput").value || "";
+  const orderValue = document.querySelector("#orderInput").value || "";
+  const colorValue = document.querySelector("#colorInput").value || "";
+  const orientationValue =
+    document.querySelector("#orientationInput").value || "";
 
   if (keywordValue) {
     getPhotos(
